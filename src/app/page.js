5c68ba427dc895b7b607ee339fa5c4dd1b452df8
unknown <omp13821380@gmail.com>
2025-07-04 12:34:05 +0330

    button added to form and also sumbitHandler created and it does not do anything

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
 'use client'
 import { useState } from "react";
 import CInput from "./components/Input";
+import { CButton } from "./components/Button";
 
 export default function Home() {
   const [username,setUsername] = useState('')
   const [password,setPassword] = useState('')
+  function submitHandler(){
+    if(username.length <= 12 && password.length <= 12){
+      console.log('submited')
+      // we have submit information and we can use it here
+    }
+  }
   return (
     <div className="w-full h-[100vh] bg-[#8007f0] flex items-center justify-center">
       <div className="min-w-72 bg-white rounded-lg p-4">
@@ -15,6 +22,7 @@ export default function Home() {
         <div className="flex flex-col gap-2">
           <CInput value={username} setValue={setUsername} type={'text'}/>
           <CInput value={password} setValue={setPassword} type={'password'}/>
+          <CButton submitHandler={submitHandler} />
         </div>
       </div>
     </div>
